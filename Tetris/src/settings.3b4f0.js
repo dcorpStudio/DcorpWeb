c0df3ca23822dda3815adcdb255f7c8b86618d3d
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-07 20:24:20 +0700

    Fixed nountris

--- Tetris/src/settings.3b4f0.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"52d36",resources:"ea96f",main:"0c148"}};
---
+++ Tetris/src/settings.efddd.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"52d36",resources:"ea96f",main:"3a725"}};
