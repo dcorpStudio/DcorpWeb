1b3702c3bf5a43306b8487946c7c8e6f0159867f
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-09 02:59:20 +0700

    Updated Oneline added tutorial

---
+++ OneLine/src/settings.c669a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","line","point"],collisionMatrix:[[true,true,true],[true,true,true],[true,true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/home.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"468ae",resources:"759f2",main:"16bc0"}};
