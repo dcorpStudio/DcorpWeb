9fdca4c66d5e8d07f757c323be83c5bff9ff56df
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-11 03:58:11 +0700

    Done updating slideWordle

--- SlideWordle/v3/src/settings.31784.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",resources:"1b2f5",main:"3cec0"}};
