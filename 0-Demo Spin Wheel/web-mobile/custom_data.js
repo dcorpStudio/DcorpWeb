e58b7d1cdf7a135dade048665944d3c0594f23eb
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-29 11:07:46 +0700

    Added demo for custom data spin wheel

---
+++ 0-Demo Spin Wheel/web-mobile/custom_data.js
@@ -0,0 +1,12 @@
+window._WHEEL_DATA_ARR = [
+    ['Label- 1', '#ffcc00'],
+    ['Label- 2', '#ffffcc'],
+    ['Label- 3', '#336699'],
+    ['Label- 4', '#ff0000'],
+    ['Label- 5', '#ffccee'],
+    ['Label- 6', '#ffcc00'],
+    ['Label- 7', '#ffffcc'],
+    ['Label- 8', '#336699'],
+    ['Label- 9', '#ff0000'],
+    ['Label- 10', '#ffccee'],
+];
---
+++ 0-Demo Spin Wheel/web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"",jsList:[]};
