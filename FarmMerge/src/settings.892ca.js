8275b37213f0c96cde6e0972c05b26037c58832b
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-30 09:44:17 +0700

    Fixed farm battle screen fit

--- FarmMerge/src/settings.892ca.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",resources:"0451a",main:"c79e7"}};
