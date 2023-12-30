307edc310414974e70704e22306d4923ab1ce658
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-30 10:24:13 +0700

    Fixed Oneline rqquest full screen

--- OneLine/src/settings.01f10.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","line","point"],collisionMatrix:[[true,true,true],[true,true,true],[true,true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/home.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"468ae",resources:"759f2",main:"7ee76"}};
