86a2e37b562cfae599fe280186971d15845cdb84
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2023-12-30 10:39:09 +0700

    Fixed pin2Circle request full screen

---
+++ PinToCircle/src/settings.57db6.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","Rotator","PinPinch"],collisionMatrix:[[true,true],[true,false],[false,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MainMenu.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"620a7",resources:"d8bb0",main:"1aa0d"}};
