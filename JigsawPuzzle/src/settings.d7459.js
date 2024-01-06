32a779354edf4c1de17c24448ae10205c3deeaef
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2024-01-06 11:08:59 +0700

    testing performance of jigsaw

--- JigsawPuzzle/src/settings.d7459.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","scrollview"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"d945e",main:"27d1d"}};
