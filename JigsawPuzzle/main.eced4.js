c1fc6290207ac9ab361ace07d1318d098ea8717a
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2024-01-10 21:17:13 +0700

    Updated jigsaw

--- JigsawPuzzle/main.eced4.js
+++ JigsawPuzzle/main.eced4.js
@@ -123,7 +123,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.cf463.js');
+        require('src/settings.d083b.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -131,7 +131,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.cf463.js');
+        require('src/settings.d083b.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
