5ffd2fbac2ac3b6fead528f6ef7ce10e4e1dace2
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2024-01-12 08:26:17 +0700

    jigsaw

--- JigsawPuzzle/src/settings.025ce.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","scrollview"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"49c4b",main:"53663"}};
