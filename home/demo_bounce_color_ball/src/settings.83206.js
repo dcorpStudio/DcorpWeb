03daa3406c8deca7a3f883f062bd5544f9dc131c
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2024-01-18 20:19:03 +0700

    boucne color ball

--- home/demo_bounce_color_ball/src/settings.83206.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"45ede",main:"26fc8"}};
