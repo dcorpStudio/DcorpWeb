93e9178281431852f31d6372d55a0d9afd7c1a11
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2024-01-19 21:01:15 +0700

    BOUNCE COLOR BALL PERFECTTTTTTTTTTTTTTTTTT !!!!

--- home/demo_bounce_color_ball/src/settings.2e8f6.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","graphics"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",resources:"aa37c",main:"44a83"}};
