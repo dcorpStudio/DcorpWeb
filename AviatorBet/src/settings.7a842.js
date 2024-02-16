3bfe202dea2c06012229d5caff8ecf77e748e7ab
dcorpStudio <48520107+dcorpStudio@users.noreply.github.com>
2024-02-17 03:07:25 +0700

    aviator

--- AviatorBet/src/settings.7a842.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","scrollview"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"7365f",resources:"fd15c",main:"c072e"}};
